00191a7c86fdac7ab1591b0c680c4f72a1990078
samG87 <samuelgomezcalcerrada87@iesamachado.org>
2024-02-01 13:24:30 +0100

    jj

--- java/validar.js
+++ java/validar.js
@@ -1,7 +1,7 @@
 function validar(elementos){
     let estancorrectos = true;
     for (var i=0;i<elementos.length; i++){
-        if (elementos[1].value=="" || elementos[1]){
+        if (elementos[1].value=="" || elementos[1].value=="off"){
             document.getElementById("campo"+(i+1).toString()).innerHTML = "El campo " + elementos[i].id + " esta vacio";
             
             estancorrectos = false;
