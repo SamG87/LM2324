b90e339ccd1696296cde0d388f585b5788fcc3c0
samG87 <samuelgomezcalcerrada87@iesamachado.org>
2024-02-01 13:23:52 +0100

    jj

---
+++ java/validar.js
@@ -0,0 +1,13 @@
+function validar(elementos){
+    let estancorrectos = true;
+    for (var i=0;i<elementos.length; i++){
+        if (elementos[1].value=="" || elementos[1]){
+            document.getElementById("campo"+(i+1).toString()).innerHTML = "El campo " + elementos[i].id + " esta vacio";
+            
+            estancorrectos = false;
+        }
+    }
+
+
+    return estancorrectos;
+}
