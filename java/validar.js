07a120ab9d9d7c2abfda88547f8356724cca03cc
samG87 <samuelgomezcalcerrada87@iesamachado.org>
2024-02-02 13:52:11 +0100

    jj

--- java/validar.js
+++ java/validar.js
@@ -1,13 +1,30 @@
 function validar(elementos){
-    let estancorrectos = true;
-    for (var i=0;i<elementos.length; i++){
-        if (elementos[1].value=="" || elementos[1].value=="off"){
-            document.getElementById("campo"+(i+1).toString()).innerHTML = "El campo " + elementos[i].id + " esta vacio";
-            
-            estancorrectos = false;
+    let estanCorrectos = true;
+    for (var i=0;i<elementos.length;i++){
+        document.getElementById("campo"+(i+1).toString()).innerHTML = "";        
+        if (elementos[i].value == "" || (i==4 && !elementos[i].checked)){
+            document.getElementById("campo"+(i+1).toString()).innerHTML = "El campo " + elementos[i].id + " está vacío";
+            estanCorrectos = false;
         }
+        
     }
-
-
-    return estancorrectos;
+    if (!validarEmail()){
+        document.getElementById("campo3").innerHTML = "Email no válido";        
+        estanCorrectos = false;
+    }
+    return estanCorrectos;
 }
+/* Función validarEmail tomada de:
+* https://www.coderbox.net/blog/validar-email-usando-javascript-y-expresiones-regulares/
+*/
+function validarEmail(){              
+	var valido;
+	var emailField = document.getElementById('email');
+	var validEmail =  /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
+	if( validEmail.test(emailField.value) ){
+		valido=true;
+	}else{
+        valido=false;
+	}
+    return valido;
+} 
